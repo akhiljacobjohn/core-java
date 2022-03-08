86dfe17dd3b7c6818e2346a26e448cc8620e854b
akhiljacobjohn <akhil_john_jkb@outlook.com>
2022-03-08 17:48:37 +0530

    Add More Classes

--- src/core/FunctionalInterfaceExample.java
+++ src/core/FunctionalInterfaceExample.java
@@ -5,11 +5,11 @@ public interface FunctionalInterfaceExample {
 	public void execute();
 
 	public default void printmsg() {
-		System.out.println("Printing a message");
+		System.out.println("Printing a sample message");
 	}
 
 	public static void printanothermsg() {
-		System.out.println("Printing another message");
+		System.out.println("Printing another sample message");
 	}
 
 }
